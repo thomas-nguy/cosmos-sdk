0118fa5a3f54969723310fd3a8677745523a5102
Thomas Nguy <thomas.nguy@crypto.com>
2025-11-14 21:24:59 +0900

    fix cleanup

--- types/mempool/priority_nonce.go
+++ types/mempool/priority_nonce.go
@@ -262,6 +262,7 @@ func (mp *PriorityNonceMempool[C]) InsertWithGasWanted(ctx context.Context, tx s
 			)
 		}
 
+		oldKey := txMeta[C]{nonce: oldScore.nonce, priority: oldScore.priority, sender: oldScore.sender, weight: oldScore.weight}
 		mp.logger.Warn("Remove old tx")
 		mp.priorityIndex.Remove(txMeta[C]{
 			nonce:    nonce,
@@ -269,16 +270,16 @@ func (mp *PriorityNonceMempool[C]) InsertWithGasWanted(ctx context.Context, tx s
 			priority: oldScore.priority,
 			weight:   oldScore.weight,
 		})
+
+		// delete from index
+		mp.logger.Warn("del from replacement", "nonce", oldScore.nonce, "priority", oldScore.priority, "sender", oldScore.sender, "weight", oldScore.weight)
+		mp.priorityIndex.Remove(oldKey)
+		mp.senderIndices[sender].Remove(oldKey)
 		mp.logger.Warn("Count down old priority", "old count", mp.priorityCounts[oldScore.priority], "new count", mp.priorityCounts[oldScore.priority]-1)
 		mp.priorityCounts[oldScore.priority]--
 		if mp.priorityCounts[oldScore.priority] == 0 {
 			delete(mp.priorityCounts, oldScore.priority)
 		}
-
-		// delete from index
-		oldKey := txMeta[C]{nonce: oldScore.nonce, priority: oldScore.priority, sender: oldScore.sender}
-		senderIndex.Remove(oldKey)
-		mp.priorityIndex.Remove(oldKey)
 	}
 
 	mp.logger.Warn("Count up new priority", "old count", mp.priorityCounts[priority], "new count", mp.priorityCounts[priority]+1)
